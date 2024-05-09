ed6e75175070036a278d26fb5897e91697836e06
abimbola7 <abimbolababajide6@gmail.com>
2024-05-09 14:19:14 +0100

    Update hero and layout components

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`min-h-screen cursor-none transition duration-300`}>
+      <body className={`min-h-screen cursor-none transition duration-300 overflow-hidden`}>
         <ScrollProvider>
           <MouseProvider>
             {children}
