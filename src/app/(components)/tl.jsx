f762059b464db0d5fe1bde5850abd809add22063
abimbola7 <abimbolababajide6@gmail.com>
2024-05-13 05:37:54 +0100

    Update tl.jsx and layout.js

--- src/app/(components)/tl.jsx
+++ src/app/(components)/tl.jsx
@@ -1,13 +1,77 @@
 "use client"
+import Image from 'next/image'
 import React from 'react'
 import { TiArrowRight } from 'react-icons/ti'
 
 const Tl = () => {
   return (
     <>
-      <div className=''>
-
+      {/* smaller screens */}
+      <div className='tab:hidden w-full grid grid-cols-2 mt-40 px-10'>
+        <div className="p-3 font-light text-white">
+          <h1 className='text-lg'>Day 1</h1>
+          <p className='text-[20px] tracking-widest font-normal '>The Baseline</p>
+          <Image
+          className='pt-10 pb-5'
+          src={"https://assets-global.website-files.com/62bac7754ea6d7967db80305/62bac7754ea6d715f6b803b9_timeline1.svg"}
+          alt=''
+          width={200}
+          height={50}
+          quality={"1000"}
+          />
+          <p className="border-none text-lg tracking-wider font-light text-white ">
+            Within 1-2 business days, we’ll reach out to you and schedule a call to discuss the details.
+          </p>
+        </div>
+        <div className="p-3 font-light text-white">
+          <h1 className='text-lg'>Day 1</h1>
+          <p className='text-[20px] tracking-widest font-normal '>The Baseline</p>
+          <Image
+          className='pt-10 pb-5'
+          src={"https://assets-global.website-files.com/62bac7754ea6d7967db80305/62bac7754ea6d715f6b803b9_timeline1.svg"}
+          alt=''
+          width={200}
+          height={50}
+          quality={"1000"}
+          />
+          <p className="border-none text-lg tracking-wider font-light text-white ">
+            Within 1-2 business days, we’ll reach out to you and schedule a call to discuss the details.
+          </p>
+        </div>
+        <div className="p-3 font-light text-white">
+          <h1 className='text-lg'>Day 1</h1>
+          <p className='text-[20px] tracking-widest font-normal '>The Baseline</p>
+          <Image
+          className='pt-10 pb-5'
+          src={"https://assets-global.website-files.com/62bac7754ea6d7967db80305/62bac7754ea6d715f6b803b9_timeline1.svg"}
+          alt=''
+          width={200}
+          height={50}
+          quality={"1000"}
+          />
+          <p className="border-none text-lg tracking-wider font-light text-white ">
+            Within 1-2 business days, we’ll reach out to you and schedule a call to discuss the details.
+          </p>
+        </div>
+        <div className="p-3 font-light text-white">
+          <h1 className='text-lg'>Day 1</h1>
+          <p className='text-[20px] tracking-widest font-normal '>The Baseline</p>
+          <Image
+          className='pt-10 pb-5'
+          src={"https://assets-global.website-files.com/62bac7754ea6d7967db80305/62bac7754ea6d715f6b803b9_timeline1.svg"}
+          alt=''
+          width={200}
+          height={50}
+          quality={"1000"}
+          />
+          <p className="border-none text-lg tracking-wider font-light text-white ">
+            Within 1-2 business days, we’ll reach out to you and schedule a call to discuss the details.
+          </p>
+        </div>
+        
       </div>
+
+      {/* laptop screens */}
       <div className='w-full hidden tab:grid grid-cols-6 px-10 gap-x-1 mt-40'>
         <div className="col-span-1 borde grid grid-rows-2">
           <div className="diamond">
