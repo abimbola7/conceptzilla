083fcbe9390fffdb66d4f3b6e3ed00f7c0ea3b52
abimbola7 <abimbolababajide6@gmail.com>
2024-05-14 18:22:33 +0100

    Refactor UI layout and styles in addons and rules components

--- src/app/(components)/addons.jsx
+++ src/app/(components)/addons.jsx
@@ -18,11 +18,10 @@ const Addons = () => {
       setAddons(data)
     })
   }, [])
-  console.log(addons)
   return (
-    <div className={`min-h-screen w-full px-3 py-32 text-white ${londrina_solid.className}`}>
-      <div className="flex flex-row max-w-7xl mx-auto space-x-10">
-        <div className='h-screen w-[35%] flex flex-col space-y-16'>
+    <div className={`w-full px-3 py-32 text-white ${londrina_solid.className} mb-20 min-h-screen`}>
+      <div className="flex flex-col tab:flex-row max-w-3xl tab:max-w-6xl mx-auto gap-y-20 tab:space-x-20">
+        <div className='w-full tab:w-[35%] flex flex-col space-y-16'>
           <h1 className={`${londrina_solid.className} text-7xl tracking-wide`}>
             <span className={`${londrina.className} block`}>
               DESIGN <TiArrowRightOutline className='inline-block mb-2' />
@@ -41,18 +40,49 @@ const Addons = () => {
           />
         </div>
 
-        <div className='h-screen w-[65%] border-none grid grid-cols-2 gap-6'>
+        <div className='w-full tab:w-[65%] border-none grid grid-cols-1 sm:grid-cols-2 gap-6'>
           {
             addons.length > 0 && addons.map((addon, i)=>(
-              <div className='bg-[#131213] rounded-2xl p-6 space-y-4' key={i}>
-                <h1 className='text-2xl font-[400] tracking-wider'>
+              <div className='bg-[#131213] rounded-2xl p-5 space-y-4 relative' key={i}>
+                {
+                  addon.premium && 
+                  <div className="absolute -top-5  rounded-2xl w-full h-full -z-[1] left-0 bg-[#F4C644] text-center tracking-wider text-[#333333]">
+                    PREMIUM
+                  </div>
+                }
+                <h1 className='text-2xl font-[400] tracking-wider !m-0'>
                   { addon.title }
                 </h1>
-                <p className='text-[#858479] text-xl tracking-wider font-light'>
-                  {
-                    addon.description
-                  }
-                </p>
+                <div className="min-h-28">
+                  <p className='text-[#858479] text-lg tracking-wider font-light'>
+                    {
+                      addon.description
+                    }
+                  </p>
+                </div>
+
+                <div className='border-t pt-3 border-t-[#262524] text-[#858479] text-lg tracking-wider font-light space-y-3'>
+                  <div className="flex flex-row justify-between">
+                    <p>
+                      Estimated time 
+                    </p>
+                    <p className='text-white'>
+                      {
+                        addon.est_time
+                      }
+                    </p>
+                  </div>
+                  <div className="flex flex-row justify-between">
+                    <p>
+                      Price
+                    </p>
+                    <p className='text-white'>
+                      ${
+                        addon.price
+                      }
+                    </p>
+                  </div>
+                </div>
               </div>
             ))
           }
